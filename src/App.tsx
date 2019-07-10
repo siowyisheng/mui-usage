cd8a30b3cbee0d342880cb79ea3eb4079ab66547
Yisheng <yisheng@Yishengs-MacBook-Pro.local>
2019-07-10 22:32:45 +0800

    feat: adds ThemeProvider, and some content

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,26 @@ import logo from './logo.svg'
 import './App.css'
 import red from '@material-ui/core/colors/red'
 import sample from 'lodash/sample'
-import { Color } from '@material-ui/core'
+import {
+  Button as MuiButton,
+  Color,
+  Paper,
+  Theme,
+  Typography
+} from '@material-ui/core'
 import Button from './Button'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import NestedSelector from './NestedSelector'
 import { StylesFromProps } from './StylesFromProps'
+import { ThemeProvider, makeStyles } from '@material-ui/styles'
+import { theme } from './theme'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    padding: 16,
+    maxWidth: 600
+  }
+}))
 
 const possibleShades = [
   '50',
@@ -39,26 +54,52 @@ const App: React.FC = () => {
     return () => clearInterval(interval)
   })
   const color = red[shade]
+  const classes = useStyles()
   return (
-    <div className='App'>
-      <header
-        css={{
-          backgroundColor: color,
-          minHeight: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontSize: 'calc(10px + 2vmin)',
-          color: 'white'
-        }}
-      >
-        <img src={logo} className='App-logo' alt='logo' />
-        <Button />
-        <NestedSelector css={{ marginTop: 8 }} />
-        <StylesFromProps css={{ marginTop: 8 }} />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <header
+          css={{
+            backgroundColor: color,
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: 'calc(10px + 2vmin)',
+            color: 'white'
+          }}
+        >
+          <img src={logo} className='App-logo' alt='logo' />
+          <Paper classes={classes}>
+            <Typography>
+              First, we create a theme with createMuiTheme.
+            </Typography>
+            <Typography>
+              The theme allows us to configure Mui components globally. We can:
+            </Typography>
+            <Typography>- override default styles.</Typography>
+            <Typography>- provide default props.</Typography>
+            <Typography>
+              - change primary, secondary and error colors.
+            </Typography>
+            <Typography>- change fonts.</Typography>
+            <Typography>
+              We pass our created theme to ThemeProvider, which we will wrap
+              around the entire app. When we want to override styling, we can
+              makeStyles to create a hook, useStyles, which when run, returns
+              `classes` which we can pass into the customized components.
+            </Typography>
+          </Paper>
+          <Button />
+          <MuiButton color='primary' variant='contained'>
+            Just a button
+          </MuiButton>
+          <NestedSelector css={{ marginTop: 8 }} />
+          <StylesFromProps css={{ marginTop: 8 }} />
+        </header>
+      </div>
+    </ThemeProvider>
   )
 }
 
--- src/NestedSelector.tsx
+++ src/NestedSelector.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
 import Paper from '@material-ui/core/Paper'
+import { Theme } from '@material-ui/core'
+import clsx from 'clsx'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     padding: 16,
-    color: 'red',
+    color: theme.palette.primary.main,
     '& p': {
       color: 'green',
       '& span': {
@@ -13,13 +15,13 @@ const useStyles = makeStyles({
       }
     }
   }
-})
+}))
 
 export default function NestedSelector({ className }: { className?: string }) {
   const classes = useStyles()
 
   return (
-    <Paper className={`${classes.root} ${className}`}>
+    <Paper className={clsx(classes.root, className)}>
       This is red since it is inside the paper.
       <p>
         This is green since it is inside the paragraph{' '}
